acaf4480c3ccb254a14aded533cd27505b712765
Gonzalo Yustos <mgy.yustos@gmail.com>
2022-07-29 15:59:00 -0300

    Primer commit backend

---
+++ src/main/java/com/portfolio/mgy/MgyApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/mgy/MgyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
